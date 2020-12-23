96ea95118c8a3707379de85b55a2b47e850d4d1f
A.S <abai.sultanov79@gmail.com>
2020-12-23 22:06:48 +0600

    no jquery slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,37 @@
 "use strict";
 
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        dots: true,
-        speed: 1000,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow_left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow_right.png"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
-    });
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         dots: true,
+//         speed: 1000,
+//         adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.png"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png"></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                     dots: true,
+//                     arrows: false
+//                 }
+//             }
+//         ]
+//     });
+// });
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
 });
