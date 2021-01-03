3300e3c16865ddd3085025301bdb691cae75e01b
A.S <abai.sultanov79@gmail.com>
2021-01-03 21:39:54 +0600

    gulpfile work

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,19 @@ const gulp  = require('gulp'),
       sass = require('gulp-sass'),
       rename = require("gulp-rename"),
       autoprefixer = require('gulp-autoprefixer'),
-      cleanCSS = require('gulp-clean-css');
+      cleanCSS = require('gulp-clean-css'),
+      imagemin = require('gulp-imagemin'),
+      htmlmin = require('gulp-htmlmin');
 
 // Static server
 gulp.task('server', function() {
     browserSync.init({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
+
+    gulp.watch('src/*.html').on("change", browserSync.reload);
 });
 
 gulp.task('styles', function() {
@@ -25,13 +29,45 @@ gulp.task('styles', function() {
                 cascade: false
             }))
             .pipe(cleanCSS({compatibility: 'ie8'}))
-            .pipe(gulp.dest('src/css'))
+            .pipe(gulp.dest('dist/css'))
             .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch('src/sass/**/*.+(scss|sass)', gulp.parallel('styles'));
-    gulp.watch('src/*.html').on("change", browserSync.reload);
+    gulp.watch('src/sass/**/*.+(scss|sass|css)', gulp.parallel('styles'));
+    gulp.watch('src/*.html').on("change", gulp.parallel('html'));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('html', () => {
+    return gulp.src("src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('scripts', () => {
+    return gulp.src("src/js/**/*.js")
+        .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task('fonts', () => {
+    return gulp.src("src/fonts/**/*")
+        .pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task('icons', () => {
+    return gulp.src("src/icons/**/*")
+        .pipe(gulp.dest("dist/icons"));
+});
+
+gulp.task('mailer', () => {
+    return gulp.src("src/mailer/**/*")
+        .pipe(gulp.dest("dist/mailer"));
+});
+
+gulp.task('img', () => {
+    return gulp.src("src/img/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/img"));
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'html', 'icons', 'mailer', 'img'));
