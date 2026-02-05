559ed94274b8fab0dc0f25c4a7c2e316f11ed88e
Abai_S_Portable <abai.sultanov79@gmail.com>
2026-02-05 16:10:36 +0600

    deprecations fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 const gulp  = require('gulp'),
       browserSync = require('browser-sync'),
-      sass = require('gulp-sass'),
-      rename = require("gulp-rename"),
-      autoprefixer = require('gulp-autoprefixer'),
+      sass = require('gulp-sass')(require('sass')),
+      rename = require('gulp-rename'),
+      postcss = require('gulp-postcss'),
+      autoprefixer = require('autoprefixer'),
       cleanCSS = require('gulp-clean-css'),
-      imagemin = require('gulp-imagemin'),
-      htmlmin = require('gulp-htmlmin');
-
+      htmlmin = require('gulp-htmlmin'),
+      webpack = require('webpack-stream'),
+      imagemin = require('gulp-imagemin');
 // Static server
 gulp.task('server', function() {
     browserSync.init({
@@ -18,19 +19,17 @@ gulp.task('server', function() {
     gulp.watch('src/*.html').on("change", browserSync.reload);
 });
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
     return gulp.src('src/sass/**/*.+(scss|sass)')
-            .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-            .pipe(rename({
-                prefix: "",
-                suffix: ".min",
-            }))
-            .pipe(autoprefixer({
-                cascade: false
-            }))
-            .pipe(cleanCSS({compatibility: 'ie8'}))
-            .pipe(gulp.dest('dist/css'))
-            .pipe(browserSync.stream());
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(rename({
+            prefix: "",
+            suffix: ".min",
+        }))
+        .pipe(postcss([autoprefixer()]))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
@@ -45,29 +44,79 @@ gulp.task('html', () => {
 });
 
 gulp.task('scripts', () => {
-    return gulp.src("src/js/**/*.js")
+    return gulp.src("src/js/main.js")
         .pipe(gulp.dest("dist/js"));
 });
 
-gulp.task('fonts', () => {
-    return gulp.src("src/fonts/**/*")
-        .pipe(gulp.dest("dist/fonts"));
-});
+// gulp.task('fonts', () => {
+//    return gulp.src("src/fonts/**/*")
+//        .pipe(gulp.dest("dist/fonts"));
+//});
 
-gulp.task('icons', () => {
-    return gulp.src("src/icons/**/*")
-        .pipe(gulp.dest("dist/icons"));
-});
+// gulp.task('icons', () => {
+//     return gulp.src("src/icons/**/*")
+//         .pipe(gulp.dest("dist/icons"));
+// });
 
 gulp.task('mailer', () => {
     return gulp.src("src/mailer/**/*")
         .pipe(gulp.dest("dist/mailer"));
 });
 
-gulp.task('img', () => {
-    return gulp.src("src/img/**/*")
-        .pipe(imagemin())
-        .pipe(gulp.dest("dist/img"));
+gulp.task('img', async () => {
+    const mozjpeg = (await import('imagemin-mozjpeg')).default,
+          optipng = (await import('imagemin-optipng')).default,
+          svgo = (await import('imagemin-svgo')).default;
+
+    return gulp.src('./src/img/**/*')
+        .pipe(imagemin([
+            mozjpeg({ quality: 75, progressive: true }),
+            optipng({ optimizationLevel: 5 }),
+            svgo({
+                plugins: [
+                    { name: 'removeViewBox', active: false },
+                    { name: 'cleanupIDs', active: false }
+                ]
+            })
+        ]))
+    .pipe(gulp.dest('./dist/img/'));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'html', 'icons', 'mailer', 'img'));
+gulp.task("build-js", () => {
+    return gulp.src("./src/js/main.js")
+                .pipe(webpack({
+                    mode: 'development',
+                    output: {
+                        filename: 'main.js'
+                    },
+                    watch: true,
+                    devtool: "source-map",
+                    module: {
+                        rules: [
+                          {
+                            test: /\.m?js$/,
+                            exclude: /(node_modules|bower_components)/,
+                            use: {
+                              loader: 'babel-loader',
+                              options: {
+                                presets: [['@babel/preset-env', {
+                                    debug: true,
+                                    corejs: 3,
+                                    useBuiltIns: "usage"
+                                }]]
+                              }
+                            }
+                          }
+                        ]
+                      }
+                }))
+                .pipe(gulp.dest('./dist/js'))
+                .on("end", browserSync.reload);
+});
+
+gulp.task('build', gulp.parallel('build-js'));
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 
+//    'fonts',
+'html', 
+// 'icons',
+ 'mailer', 'img', "build"));
